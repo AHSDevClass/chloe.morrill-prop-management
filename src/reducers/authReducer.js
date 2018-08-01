44b1f943e456a92457ead01a92d10d10b0df99fe
Chloe Morrill <chloemorrill@Chloes-MacBook-Pro.local>
2018-07-31 20:12:42 -0600

    built signin functionality

--- src/reducers/authReducer.js
+++ src/reducers/authReducer.js
@@ -7,12 +7,13 @@ const INITIAL_STATE = {
     user: []
 }
 
-export default function(state = INITIAL_STATEs, action) {
+export default function(state = INITIAL_STATE, action) {
     switch (action.type) {
         case AUTHENTICATE_USER:
         const { user } = action.payload;
             return {
                 ...state,
+                authenticated: true,
                 user
             }
     
