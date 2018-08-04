bb1674081ec00d712b13897689a38eb97bc3c1f7
Chloe Morrill <chloemorrill@Chloes-MacBook-Pro.local>
2018-08-04 17:42:16 -0600

    changed styles based on selectedrequesttype in store and fixed payload bug on the changeselectedrequesttype

--- src/components/requests/requestsBox.js
+++ src/components/requests/requestsBox.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import * as actions from '../../actions';
 
 class RequestsBox extends Component {
     render() {
         const { count, title } = this.props;
+        const className = `requests-box ${this.props.selectedRequestType == title ? 'requests-box-active' : 'requests-box-inactive'}`
         return (
-            <a onClick={(title) => this.props.changeSelectedRequestType(title)} className='requests-box'>
+            <a onClick={() => this.props.changeSelectedRequestType(title)} className={className}>
                 <div className='requests-box__count'>{count}</div>
                 <div className='requests-box__title'>{title}</div>
                 <div className='requests-box__point'></div>
@@ -13,4 +16,11 @@ class RequestsBox extends Component {
     }
 }
 
-export default RequestsBox;
+function mapStateToProps(state) {
+    const { selectedRequestType } = state.requests;
+    return {
+        selectedRequestType
+    };
+}
+
+export default connect(mapStateToProps, actions)(RequestsBox);
