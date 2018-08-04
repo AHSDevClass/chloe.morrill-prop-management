8d82f861a6cad49677390ba7368d48d5ad0b3642
Chloe Morrill <chloemorrill@Chloes-MacBook-Pro.local>
2018-08-04 15:48:02 -0600

    built requests box component and built a grid for requestsboxes

--- src/components/requests/requestsBoxes.js
+++ src/components/requests/requestsBoxes.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
+import RequestsBox from './requestsBox';
 
 class RequestsBoxes extends Component {
     render() {
         return (
             <div className='requests-boxes'>
-                {/* pending */}
-                {/* in progress */}
-                {/* complete */}
+                <RequestsBox title={'Pending'} count={2}/>
+                <RequestsBox title={'In-Progress'} count={5}/>
+                <RequestsBox title={'Complete'} count={1}/>
             </div>
         )
     }
