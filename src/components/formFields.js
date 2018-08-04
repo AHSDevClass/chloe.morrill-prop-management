b189b57a8fed367419913ffe4ea79c66315b051e
Chloe Morrill <chloemorrill@Chloes-MacBook-Pro.local>
2018-08-04 10:45:42 -0600

    mapped newsletter edit data into form

--- src/components/formFields.js
+++ src/components/formFields.js
@@ -2,7 +2,8 @@ import React, { Component } from 'react';
 
 export class FormInput extends Component {
     render() {
-        const { className, title, input, type, placeholder } = this.props;
+        const { className, title, input, type, placeholder, editValue } = this.props;
+
         return (
             <div className={`${className} form-input`}>
                 <label className='form-input__title'>{title}</label>
@@ -11,6 +12,7 @@ export class FormInput extends Component {
                     type={type}
                     {...input}
                     placeholder={placeholder}
+                    value={editValue ? editValue : input.value}
                 />
             </div>
         )
@@ -19,7 +21,8 @@ export class FormInput extends Component {
 
 export class FormTextArea extends Component {
     render() {
-        const { className, title, input, type, placeholder } = this.props;
+        const { className, title, input, type, placeholder, editValue } = this.props;
+
         return (
             <div className={`${className} form-textarea`}>
                 <label className='form-textarea__title'>{title}</label>
@@ -28,6 +31,7 @@ export class FormTextArea extends Component {
                     type={type}
                     {...input}
                     placeholder={placeholder}
+                    value={editValue ? editValue : ''}
                 >
                 
                 </textarea>
@@ -65,7 +69,8 @@ export class FormImage extends Component {
     }
 
     render() {
-        const { className, title, input, type, imageUrl } = this.props;
+
+        const { className, title, input, imageUrl } = this.props;
         return (
             <div className={`${className} form-image`}>
                 <label className='form-image__title'>{title}</label>
