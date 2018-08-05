d983f98990dbb5ae24b1894b1adfdc066f4127f3
Chloe Morrill <chloemorrill@Chloes-MacBook-Pro.local>
2018-08-05 09:27:10 -0600

    JSX for request item

--- src/components/requests/requestsItem.js
+++ src/components/requests/requestsItem.js
@@ -1,13 +1,23 @@
 import React, { Component } from 'react';
 import Icon from '../icon';
+import Button from '../button';
 
 class RequestsItem extends Component {
     render() {
         return (
             <div className='requests-item'>
-                <div>
-                    <Icon icon='fas fa-exclamation-triangle'/>
+                <Icon className='requests-item__icon' icon='fas fa-exclamation-triangle'/>
+                <div className='requests-item__title'>
+                    Paint coming off door.
                 </div>
+                <div className='requests-item__tenant-unit'>
+                    Hugs-a-lot - Unit 821719
+                </div>
+                <Icon className='requests-item__arrow' icon='fas fa-sort-down'/>
+                <div className='requests-item__date'>
+                    Today
+                </div>
+                <Button className='requests-item__move' icon='fas fa-wrench' callback={() => console.log('trying to change request status')}/>
             </div>
         )
     }
