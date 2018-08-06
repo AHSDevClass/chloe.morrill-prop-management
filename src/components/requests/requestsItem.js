f0267fa01113e9381531c0ded17555b72d732c0c
Chloe Morrill <chloemorrill@Chloes-MacBook-Pro.local>
2018-08-05 20:10:12 -0600

    cleaned up request item grid

--- src/components/requests/requestsItem.js
+++ src/components/requests/requestsItem.js
@@ -8,12 +8,12 @@ class RequestsItem extends Component {
             <div className='requests-item'>
                 <Icon className='requests-item__icon' icon='fas fa-exclamation-triangle'/>
                 <div className='requests-item__title'>
-                    Paint coming off door.
+                    <div className='requests-item__title__text'>Paint coming off door.</div>
+                    <Icon className='requests-item__title__arrow' icon='fas fa-sort-down'/>
                 </div>
                 <div className='requests-item__tenant-unit'>
                     Hugs-a-lot - Unit 821719
                 </div>
-                <Icon className='requests-item__arrow' icon='fas fa-sort-down'/>
                 <div className='requests-item__date'>
                     Today
                 </div>
